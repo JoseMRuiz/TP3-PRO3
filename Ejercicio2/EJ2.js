882346fa2c674b7d7f2f497737c595c64444a9d7
JoseMRuiz <josemruiz6@gmail.com>
2025-06-27 17:30:05 -0300

    Ejercicio 2 correcion

--- Ejercicio2/EJ2.js
+++ Ejercicio2/EJ2.js
@@ -1,9 +1,5 @@
 const palabras = ["manzana", "banana", "pera", "durazno", "frutilla", "mango"];
 
-window.onload = function() {
-    mostrarPalabras(palabras);
-};
-
 
 function mostrarPalabras(lista) {
     const listaFrutas = document.getElementById("listado")
@@ -15,7 +11,8 @@ function mostrarPalabras(lista) {
     });
 }
 
-function filtrarPalabras () {
+function filtrarPalabras (event) {
+    event.preventDefault();
     const input = document.getElementById("filtro").value.trim().toLowerCase();
     const mensajeError = document.getElementById("mensaje");
 
@@ -28,6 +25,12 @@ mensajeError.textContent="";
 
 const palabrasFiltradas = palabras.filter(palabra => palabra.toLowerCase().includes(input));
 
+document.getElementById("filtro").value = "";
 
 mostrarPalabras(palabrasFiltradas);
+
 }
+
+mostrarPalabras(palabras);
+const formulario = document.getElementById("formulario")
+formulario.addEventListener("submit", filtrarPalabras);
